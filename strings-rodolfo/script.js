82dcfb7ed6f6ee1e850488983f91223240087e59
rodolfopx <nairnei.junior@escola.pr.gov.br>
2025-09-26 16:51:13 -0300

    Add files via upload

---
+++ strings-rodolfo/script.js
@@ -0,0 +1,27 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+    
+    campoResultado.textContent = palavras;
+}
+
+function processaTexto (texto){
+    let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = [];
+    for(let i of palavras){
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias [i]++;
+            }
+        }
+    }        
+    console.log(frequencias);
+    return palavras;
+    }
